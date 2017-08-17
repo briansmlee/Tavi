e7f79c19306f0997c372f692e67016da7846dfbe
Brian Lee <briansmlee@gmail.com>
2017-08-17 11:43:42 +0900

    selecting info to store from trello cards

--- store.py
+++ store.py
@@ -2,13 +2,12 @@
 # 참조: https://github.com/bmccormack/trello-python-demo/blob/master/demo.py#L54
 
 import requests           # a 3rd party library. `pip install requests`
-#import json               # all responses from Trello come back as JSON
+import json               # all responses from Trello come back as JSON
 from pprint import pprint # this is a handy utility for printing dictionaries in a human readable way
 
 
-### Getting an API Key and Token
-# need to use OAuth to hide key and token
-# get key and token from the settings file.
+# TODO: need to use OAuth to hide key and token
+# currently gets key and token from the settings file.
 key = ''
 token = ''
 
@@ -20,7 +19,6 @@ if not key or token:
 
 
 ### GET request to the Trello API ###
-#
 # We're going to make a GET request to the Trello API and print out a list of the boards
 # to which we belong.
 #
@@ -28,7 +26,8 @@ if not key or token:
 base = 'https://trello.com/1/'
 
 # Build out the URL based on the documentation
-boards_url = base + 'members/me/boards'
+# boards_url = base + 'members/me/boards'
+boards_url = base + 'members'
 
 # Let's store our API key and token as parameters
 params_key_and_token = {'key':key,'token':token}
@@ -49,22 +48,22 @@ response = requests.get(boards_url, params=params_key_and_token, data=arguments)
 # The following will give us an array of dictionaries
 response_array_of_dict = response.json()
 
-#
 # print("pretty printed:\n")
 # pprint(response_array_of_dict)
 
-print("아래의 보드 중 카드 데이터를 추출할 보드를 선택해주세요.")
+print("아래의 보드 중 카드 데이터를 추출할 보드를 선택해주세요:\n")
 
 # Let's go ahead and iterate through the list of boards and print the name of each board
 for idx, board in enumerate(response_array_of_dict):
-    print("%d: %s" % idx, board['name'])
+    print("%d: %s" % (idx, board['name']))
 
-choice = input("\n선택한 보드에 해당하는 숫자를 적어주세요: ")
+choice = int(input("\n선택한 보드에 해당하는 숫자를 적어주세요: "))
 board = response_array_of_dict[choice]
 board_id = board['id']
 
 cards_url = base + 'boards/' + board_id + '/cards'
 
+# cards in target board. (list of dicts)
 resp = requests.get(cards_url, params=params_key_and_token, data=arguments).json()
 
 pprint(resp)
