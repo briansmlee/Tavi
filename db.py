e1e7f060fdee87cce98e5efb20f3372bab75cec5
Brian Lee <briansmlee@gmail.com>
2017-08-17 13:50:12 +0900

    f'n to add dict Trello card to sqlalchemy

--- db.py
+++ db.py
@@ -6,47 +6,44 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///tutorial.db')
+engine = create_engine('sqlite:///.db')
 
 Base = declarative_base() 
-
-class Foods(Base):
-    __tablename__ = 'foods'
-    
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-    quantity = Column(Integer)
-    calories = Column(Float)
-    
-    # optional
-    def __repr__(self):
-        return "<User(name=%s, quantity=%d, calories=%f>" % \
-                self.name, self.quantity, self.calories
-
-Base.metadata.create_all(engine)
-
-ex_food = Foods(name='milk', quantity= 1, calories=120.0)
-
-print(ex_food.name)
 
 # create session
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# persist User obj by adding to Session
-session.add(ex_food)
-# now the instance is pending.
 
-session.add_all([
-    Foods(name='avocado', quantity=1, calories = 100.0)
-])
 
-session.commit()
+# class for table
+class Card(Base):
+    __tablename__ = 'cards'
+    
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    desc = Column(String) #description
+    
+    # optional
+    def __repr__(self):
+        return "<Card: name=%s, descritpion=%s>" % \
+                self.name, self.desc,
 
-print(ex_food.id)
+# what is this for?
+Base.metadata.create_all(engine)
 
-for instance in session.query(Foods).order_by(Foods.id):
-    print(instance.name, instance.quantity)
+def add_card(dct):
+    """
+       adds dict card to the database
+       keys in param dict must match instance vars in Card
+    """
+    card = Card(**dct) # unfolds dict
+    session.add(card)
+    session.commit()
+
+
+for instance in session.query(Card).order_by(Card.id):
+    print(instance.name, instance.desc)
     
 
 
