7ee8be20911955bc8852270380f194a50a3f87f9
Brian Lee <briansmlee@gmail.com>
2017-08-18 17:02:59 +0900

    bug fixed: now able to create Conf page in py!

--- confluence.py
+++ confluence.py
@@ -3,18 +3,15 @@
 import requests
 import json
 import pprint
-from settings import conf_addr, auth
+from settings import conf_home, auth
 
 class Conf:
     """ Confluence class for using Server REST API """
-    base_addr = conf_addr # base address for API calls
-    addr_ = base_addr + '/confluence/rest/content/'
-    
-    # username = ''
-    # password = ''
+    base_addr = conf_home # base address for API calls
+    create_addr = base_addr + '/rest/api/content/' # addr for creating content
     
     auth_ = auth
-    # headers_ = ({'Content-Type' : 'application/json'})
+    headers_ = ({'Content-Type' : 'application/json'})
     # headers_admin = ('admin' , 'admin')
     
     def printResponse(self, r):
@@ -24,7 +21,7 @@ class Conf:
     def form_pageData(self):
         """forms page data from input"""
         data = {}
-        data['type'] = "space"
+        data['type'] = "page"
         data['title'] = "API 테스트용 페이지"
         data['space'] = {"key":"AP"}
         data['body'] = {
@@ -60,8 +57,8 @@ class Conf:
            creates page with content from input dictionary
         """
         pageData = self.form_pageData()
-        r = requests.post(Conf.addr_, data=json.dumps(pageData), 
-                auth = Conf.auth_) #headers = Conf.headers_)
+        r = requests.post(Conf.create_addr, data=json.dumps(pageData), 
+                auth = Conf.auth_, headers = Conf.headers_)
         r.json()
         print(r)
 
